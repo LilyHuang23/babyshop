87ec6159d4ca5b16bf4d89b978eec9e138559991
Lily Huang <l613426210@gmail.com>
2023-07-15 20:20:58 -0600

    get rid of auth

--- routes/index.js
+++ routes/index.js
@@ -3,17 +3,13 @@ const router = express.Router();
 const { auth, requiresAuth } = require('express-openid-connect');
 
 
+router.use('/', require('./swagger'));
 
+router.use('/product', require('./product'));
 
+router.use('/user', require('./user'));
 
+router.use('/review', require('./review'));
 
-router.use('/', requiresAuth(), require('./swagger'));
-
-router.use('/product',requiresAuth(), require('./product'));
-
-router.use('/user',requiresAuth(), require('./user'));
-
-router.use('/review',requiresAuth(), require('./review'));
-
-router.use('/cart',requiresAuth(), require('./cart'));
+router.use('/cart', require('./cart'));
 module.exports = router;
